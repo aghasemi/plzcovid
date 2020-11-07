5991bff782866a7cdddc1045476a3f88e2649946
alireza <ghasemi.a.ir@gmail.com>
2020-11-07 01:40:48 +0100

    Implemented manual caching

--- app.py
+++ app.py
@@ -6,8 +6,10 @@ from data import load_data
 # https://discuss.streamlit.io/t/custom-render-widths/81/9
 
 st.set_page_config(page_title='Evolution of Covid-19 Cases in Canton Zürich per Postal Code')
+st.markdown("<style>.reportview-container .main .block-container{ max-width: 2000px; } </style>",unsafe_allow_html=True,) #Makes the page wide
 st.title("Evolution of Covid-19 Cases in Canton Zürich per Postal Code")
 
+st.markdown('The data are provided by the [@OpenDaataZH](https://twitter.com/OpenDataZurich) in their [Covid-19 GitHub repository](https://github.com/openZH/covid_19). All the credit goes to them.')
 
 
 pd.set_option('display.max_colwidth', None)
@@ -22,14 +24,14 @@ plot_y_axis_column_min = 'NewConfCases_7days_min_per10k' if per_capita else 'New
 plot_y_axis_column_max = 'NewConfCases_7days_max_per10k' if per_capita else 'NewConfCases_7days_max'
 plot_y_axis_column_avg = 'NewConfCases_7days_avg_per10k' if per_capita else 'NewConfCases_7days_avg'
 
-st.markdown("### Please choose one or more postal codes")
+#st.markdown("### Please choose one or more postal codes")
 plzs = list(cases.groupby(['PLZ'], as_index=False).groups.keys())
-chosen_plzs = st.multiselect("",plzs,['8004','8400'])
+chosen_plzs = st.multiselect("Please choose one or more postal codes",plzs,['8004','8400'])
 
 start_date = cases.Date.iloc[0].replace(tzinfo=None)
 end_date = cases.Date.iloc[-1].replace(tzinfo=None)
-st.markdown("### Please choose the range of dates")
-dates = st.date_input("",[start_date,end_date],start_date,end_date)
+#st.markdown("### Please choose the range of dates")
+dates = st.date_input("Please choose the range of dates",[start_date,end_date],start_date,end_date)
 if len(dates)<2:
     start_date = dates[0]
 else:
@@ -38,14 +40,16 @@ else:
 cases = cases[ (cases.Date >= pd.Timestamp(start_date, tz="Europe/Paris")) & (cases.Date <= pd.Timestamp(end_date, tz="Europe/Paris"))]
 
 chart = alt.LayerChart()
-st.markdown("### Evolution of Covid-19 cases for the chosen postal codes")
+#st.markdown("### Evolution of Covid-19 cases for the chosen postal codes")
 tooltip = ['Date','PLZ',alt.Tooltip(plot_y_axis_column_min, title='Min', format='0.3'),alt.Tooltip(plot_y_axis_column_max, title='Max', format='0.3')]
 for i,plz in enumerate(chosen_plzs):
     color = colors[i]
     cases_per_plz = cases[cases.PLZ == plz]
-    st.markdown(f'#### {plz} {plz_name[plz]} is {color}')
-    chart  += alt.Chart(cases_per_plz).mark_area(opacity = 0.25,color = color).encode(x=alt.X('Date', axis=alt.Axis(labels=False)), y=alt.Y(plot_y_axis_column_min,title=plot_y_axis_title), y2=alt.Y2(plot_y_axis_column_max), tooltip = tooltip)
-    chart  += alt.Chart(cases_per_plz).mark_line(color = color).encode(x=alt.X('Date', axis=alt.Axis(labels=True)), y=alt.Y(plot_y_axis_column_avg,title=plot_y_axis_title), tooltip = tooltip)
+    chart  += alt.Chart(cases_per_plz).mark_area(opacity = 0.25,color = color).encode(x=alt.X('Date', axis=alt.Axis(labels=True, format = '%d %b')), y=alt.Y(plot_y_axis_column_min,title=plot_y_axis_title), y2=alt.Y2(plot_y_axis_column_max), tooltip = tooltip)
+    chart  += alt.Chart(cases_per_plz).mark_line(color = color).encode(x=alt.X('Date', axis=alt.Axis(labels=True, format = '%d %b')), y=alt.Y(plot_y_axis_column_avg,title=plot_y_axis_title), tooltip = tooltip)
 
 st.altair_chart(chart.configure_title(fontSize=14).configure(background='#F9F9F0') , use_container_width=True)
 
+for i,plz in enumerate(chosen_plzs):
+    color = colors[i]
+    st.markdown(f' {plz} {plz_name[plz]} is {color}')
--- data.py
+++ data.py
@@ -3,32 +3,40 @@ import streamlit as st
 import urllib.request, json 
 
 
-#@st.cache
+plz_name = None
+cases = None 
+
+#Streamlit cache doesn't work here. So will do it manually
 def load_data():
+    global plz_name
+    global cases
     #PLZ to Name Mapping
-    plz_name = dict({})
-    with urllib.request.urlopen("https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_plz/PLZ_gen_epsg4326_F_KTZH_2020.json") as url:
-        data = json.loads(url.read().decode())
-        for feature in data['features']:
-            plz = (feature['properties']['PLZ'])
-            name = feature['properties']['Ortschaftsname']
-            if plz is not None and name is not None: plz_name[str(int(plz))] = str(name)
-    plz_name['8403'] = 'Winterthur' #Some manual fixes
+    if plz_name is None:
+        plz_name = dict({})
+        with urllib.request.urlopen("https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_plz/PLZ_gen_epsg4326_F_KTZH_2020.json") as url:
+            data = json.loads(url.read().decode())
+            for feature in data['features']:
+                plz = (feature['properties']['PLZ'])
+                name = feature['properties']['Ortschaftsname']
+                if plz is not None and name is not None: plz_name[str(int(plz))] = str(name)
+        plz_name['8403'] = 'Winterthur' #Some manual fixes
 
-    cases = pd.read_csv('https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_plz/fallzahlen_kanton_ZH_plz.csv')
-    cases  = cases[cases['PLZ'].str.len() == 4] #Remove anything that is not a PLZ
-    
-    #Absolute numbers
-    cases['NewConfCases_7days_range'] = cases['NewConfCases_7days'].apply(lambda x: list(map(lambda s: int(s), str(x).split('-'))))
-    cases['NewConfCases_7days_min'] = cases['NewConfCases_7days_range'].apply(lambda a: a[0])
-    cases['NewConfCases_7days_max'] = cases['NewConfCases_7days_range'].apply(lambda a: a[1])
-    cases['NewConfCases_7days_avg'] = .5*cases['NewConfCases_7days_max']+ 0.5*cases['NewConfCases_7days_min']
+    if cases is None:
 
-    cases['NewConfCases_7days_range_per10k'] = cases.apply(lambda row: list(map(lambda x: 10000*x/row['Population'], row['NewConfCases_7days_range'])), axis=1)
-    cases['NewConfCases_7days_min_per10k'] = cases['NewConfCases_7days_range_per10k'].apply(lambda a: a[0])
-    cases['NewConfCases_7days_max_per10k'] = cases['NewConfCases_7days_range_per10k'].apply(lambda a: a[1])
-    cases['NewConfCases_7days_avg_per10k'] = .5*cases['NewConfCases_7days_max_per10k']+ 0.5*cases['NewConfCases_7days_min_per10k']
-    cases['Date'] = pd.to_datetime(cases['Date'],utc=True)
+        cases = pd.read_csv('https://raw.githubusercontent.com/openZH/covid_19/master/fallzahlen_plz/fallzahlen_kanton_ZH_plz.csv')
+        cases  = cases[cases['PLZ'].str.len() == 4] #Remove anything that is not a PLZ
+        
+        #Absolute numbers
+        cases['NewConfCases_7days_range'] = cases['NewConfCases_7days'].apply(lambda x: list(map(lambda s: int(s), str(x).split('-'))))
+        cases['NewConfCases_7days_min'] = cases['NewConfCases_7days_range'].apply(lambda a: a[0])
+        cases['NewConfCases_7days_max'] = cases['NewConfCases_7days_range'].apply(lambda a: a[1])
+        cases['NewConfCases_7days_avg'] = .5*cases['NewConfCases_7days_max']+ 0.5*cases['NewConfCases_7days_min']
+
+        cases['NewConfCases_7days_range_per10k'] = cases.apply(lambda row: list(map(lambda x: 10000*x/row['Population'], row['NewConfCases_7days_range'])), axis=1)
+        cases['NewConfCases_7days_min_per10k'] = cases['NewConfCases_7days_range_per10k'].apply(lambda a: a[0])
+        cases['NewConfCases_7days_max_per10k'] = cases['NewConfCases_7days_range_per10k'].apply(lambda a: a[1])
+        cases['NewConfCases_7days_avg_per10k'] = .5*cases['NewConfCases_7days_max_per10k']+ 0.5*cases['NewConfCases_7days_min_per10k']
+        cases['Date'] = pd.to_datetime(cases['Date'],utc=True)
     return cases, plz_name
 
 
