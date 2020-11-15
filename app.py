76e80d148a3a59a5d2fb65a9460b2b0fbb017497
alireza <ghasemi.a.ir@gmail.com>
2020-11-15 19:14:47 +0100

    Added districts

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ pd.set_option('display.max_colwidth', None)
 
     
 cases = load_data()
-st.markdown(f'_Last update: {cases.Date.max().strftime("%Y-%m-%d")}_')
+st.markdown(f'_Last data update: {cases.Date.max().strftime("%Y-%m-%d")}_')
 
 colors = ['black', 'silver', 'maroon', 'red', 'purple', 'fuchsia', 'green', 'olive', 'navy', 'orange', 'azure', 'beige', 'bisque', 'blanchedalmond', 'blueviolet', 'chartreuse', 'chocolate', 'coral', 'cornflowerblue', 'cornsilk', 'crimson', 'cyan', 'darkblue', 'darkcyan', 'darkgoldenrod', 'darkgray', 'darkgreen', 'darkgrey', 'darkkhaki', 'darkmagenta', 'darkolivegreen', 'darkorange', 'darkorchid', 'darkred', 'darksalmon', 'darkseagreen', 'darkslateblue', 'darkslategray', 'darkslategrey', 'darkturquoise', 'darkviolet', 'deeppink', 'deepskyblue', 'dimgray', 'dimgrey', 'dodgerblue', 'firebrick', 'floralwhite', 'forestgreen', 'gainsboro', 'ghostwhite', 'gold', 'goldenrod', 'greenyellow', 'grey', 'honeydew', 'hotpink', 'indianred', 'indigo', 'ivory', 'khaki', 'lavender', 'lavenderblush', 'lawngreen', 'lemonchiffon', 'lightblue', 'lightcoral', 'lightcyan', 'lightgoldenrodyellow', 'lightgray', 'lightgreen', 'lightgrey', 'lightpink', 'lightsalmon', 'lightseagreen', 'lightskyblue', 'lightslategray', 'lightslategrey', 'lightsteelblue', 'lightyellow', 'limegreen', 'linen', 'magenta', 'mediumaquamarine', 'mediumblue', 'mediumorchid', 'mediumpurple', 'mediumseagreen', 'mediumslateblue', 'mediumspringgreen', 'mediumturquoise', 'mediumvioletred', 'midnightblue', 'mintcream', 'mistyrose', 'moccasin', 'navajowhite', 'oldlace', 'olivedrab', 'orangered', 'orchid', 'palegoldenrod', 'palegreen', 'paleturquoise', 'palevioletred', 'papayawhip', 'peachpuff', 'peru', 'pink', 'plum', 'powderblue', 'rosybrown', 'royalblue', 'saddlebrown', 'salmon', 'sandybrown', 'seagreen', 'seashell', 'sienna', 'skyblue', 'slateblue', 'slategray', 'slategrey', 'snow', 'springgreen', 'steelblue', 'tan', 'thistle', 'tomato', 'turquoise', 'violet', 'wheat', 'whitesmoke', 'yellowgreen', 'rebeccapurple']
 per_capita = st.checkbox("Show cases per 10000 population")
@@ -23,10 +23,11 @@ plot_y_axis_title = 'Cases in the last 7 days per 10k population' if per_capita 
 plot_y_axis_column_min = 'NewConfCases_7days_min_per10k' if per_capita else 'NewConfCases_7days_min'
 plot_y_axis_column_max = 'NewConfCases_7days_max_per10k' if per_capita else 'NewConfCases_7days_max'
 plot_y_axis_column_avg = 'NewConfCases_7days_avg_per10k' if per_capita else 'NewConfCases_7days_avg'
+plot_y_axis_column_range = 'NewConfCases_7days_range_str_per10k' if per_capita else 'NewConfCases_7days_range_str'
 
 #st.markdown("### Please choose one or more postal codes")
-plzs = list(cases.groupby(['PLZ'], as_index=False).groups.keys())
-chosen_plzs = st.multiselect("Please choose one or more postal codes",plzs,['8004','8400'])
+postal_areas = list(cases.groupby(['Name'], as_index=False).groups.keys())
+chosen_postal_areas = st.multiselect("Please choose one or more postal codes",postal_areas,['8001 Zürich','8400 Winterthur'])
 
 start_date = cases.Date.iloc[0].replace(tzinfo=None)
 end_date = cases.Date.iloc[-1].replace(tzinfo=None)
@@ -41,15 +42,18 @@ cases = cases[ (cases.Date >= pd.Timestamp(start_date, tz="Europe/Paris")) & (ca
 
 chart = alt.LayerChart()
 #st.markdown("### Evolution of Covid-19 cases for the chosen postal codes")
-tooltip = ['Date','Name','Population',alt.Tooltip('Area',title='Area (km²)', format='0.3'),alt.Tooltip(plot_y_axis_column_min, title='Min', format='0.3'),alt.Tooltip(plot_y_axis_column_max, title='Max', format='0.3')]
+tooltip = ['Date',alt.Tooltip('Week_of_year',title='Week of year'),'Name','District','Population',alt.Tooltip('Area',title='Area (km²)', format='0.3'),
+    alt.Tooltip(plot_y_axis_column_range, title='Range'), alt.Tooltip('NewDeaths_in_district_in_week',title='Weekly deaths in district')] #,alt.Tooltip(plot_y_axis_column_min, title='Min', format='0.3'),
 x_axis = alt.X('Date', axis=alt.Axis(labels=True, format = '%d %b'))
 y_axis = alt.Y(plot_y_axis_column_avg,title=plot_y_axis_title)
-for i,plz in enumerate(chosen_plzs):
+legend = "|Area | Color | \n | - | - | \n"
+for i,plz_name in enumerate(chosen_postal_areas):
     color = colors[i]
-    cases_per_plz = cases[cases.PLZ == plz]
+    cases_per_plz = cases[cases.Name == plz_name]
     chart  += alt.Chart(cases_per_plz).mark_area(opacity = 0.25,color = color).encode(x=x_axis, y=y_axis, y2=alt.Y2(plot_y_axis_column_max), tooltip = tooltip)
-    chart  += alt.Chart(cases_per_plz).mark_line(color = color).encode(x=x_axis, y=y_axis, tooltip = tooltip)
-    st.markdown(f' {cases_per_plz.iloc[0].Name} is {color}')
+    chart  += alt.Chart(cases_per_plz).mark_line(color = color, size=4).encode(x=x_axis, y=y_axis, tooltip = tooltip)
+    legend += (f' | {cases_per_plz.iloc[0].Name} | {color} | \n')
 
-
+st.markdown(legend)
+st.markdown('Hoveryour mose on the plots for more information')
 st.altair_chart(chart.configure_title(fontSize=14).configure(background='#F9F9F0') , use_container_width=True)
